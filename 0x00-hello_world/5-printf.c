f9a9346176a0344b701412a7d3c16a2981dfdbf8
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-04-23 07:31:45 -0700

    .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a string of texts
+ * 
+ * Return: 0 on successful completion, or a non-zero value on error.
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
