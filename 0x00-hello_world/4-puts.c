9058607a6df2f47b59adc1466ac2ac29d0a2b7a8
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-04-23 06:18:22 -0700

    .

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,16 @@
  * A simple program that prints a string
  */
 int main(void)
-{//Character declaration
+{/**
+*Character declaration
+*/
 char str[] = "\"Programming is like building a multilingual puzzle";
-//output
+/**
+ * output
+ */ 
 puts(str);
-//End function
+/**
+ * End function
+ */ 
 return (0);
 }
