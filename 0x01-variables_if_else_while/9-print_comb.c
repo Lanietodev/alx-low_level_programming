ae55df365134c975e6723e3cf2fc00315d04ce91
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-04-23 19:21:59 -0700

    .

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+	{
+	int i = 0;
+
+	while (i < 10)
+	{
+	putchar(i + '0');
+	putchar(',');
+	putchar(' ');
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
