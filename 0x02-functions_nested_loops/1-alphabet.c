84bfff86b0eeb563abbb7e1696e1af869bad39ef
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-04-29 12:24:19 -0700

    .

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * main - entry point
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char i;
 
-	for (i = 'a' ; i <= 'z' ; ++i)
+	for (i = a; i <= z; ++i)
 	{
 	_putchar (i);
 	}
 	_putchar ('\n');
-	return (0);
 }
