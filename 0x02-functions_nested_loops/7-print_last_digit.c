f96e8b8c5d8b6835267c275b0e64a313fb095dcc
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-01 14:21:39 -0700

    .

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,11 @@
 /**
  * print_last_digit - entry point
  *
- * @int: param type for _abs
  * @n: value to check
  * Return: last digit (success)
 */
-int _abs(int);
 int print_last_digit(int n)
 {
-return (_abs(n) % 10);
+int last_digit = _abs(n) % 10;
+return (last_digit);
 }
