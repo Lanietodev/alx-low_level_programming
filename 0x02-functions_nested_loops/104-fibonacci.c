ba53aa838c0bd52cdce529adfc7b759e9a85bc96
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-03 04:43:47 -0700

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,7 +9,7 @@ int main(void)
 long first = 1, second = 2, next;
 int n;
 
-printf("%d, %d", first, second);
+printf("%ld, %ld", first, second);
 for (n = 0; n <= 98; n++)
 {
 next = first + second;
