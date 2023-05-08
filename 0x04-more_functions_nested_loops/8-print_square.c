f58687083cd8760a073255873796f596b0598f58
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-08 00:08:19 -0700

    modified some files

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,7 +6,7 @@
 */
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 if (size <= 0)
 {
@@ -16,8 +16,10 @@ else
 {
 for (i = 0; i < size; i++)
 {
+for (j = 0; j <= size; j++)
+{
 _putchar('#');
 }
-}
 _putchar('\n');
+}
 }
