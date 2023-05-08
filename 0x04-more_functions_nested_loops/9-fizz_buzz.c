0defcf75232159efe20f9327fc4d9b0f4f9c1898
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-08 00:15:47 -0700

    modified some files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
 * main - entry point
-* @n: parameter to check
+*
 * Return: 0 (success)
 */
-int main(int n)
+int main(void)
 {
+int n;
+
 for (n = 1; n <= 100; n++)
 {
 if (n % 3 == 0 && n % 5 == 0)
