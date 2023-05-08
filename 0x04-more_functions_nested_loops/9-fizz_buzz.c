4115d7d341e1d2965ab2ae4f03592afd22cd6720
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-08 00:22:42 -0700

    modified some files

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,14 +13,17 @@ for (n = 1; n <= 100; n++)
 if (n % 3 == 0 && n % 5 == 0)
 {
 printf("FizzBuzz");
+printf(" ");
 }
 else if (n % 3 == 0)
 {
 printf("Fizz");
+printf(" ");
 }
 else if (n % 5 == 0)
 {
 printf("Buzz");
+printf(" ");
 }
 else
 {
@@ -28,12 +31,13 @@ if (n > 9)
 {
 printf("%d", n / 10);
 printf("%d", n % 10);
+printf(" ");
 }
 else
 {
 printf("%d", n);
-}
-}
 printf(" ");
+}
+}
 }
 }
