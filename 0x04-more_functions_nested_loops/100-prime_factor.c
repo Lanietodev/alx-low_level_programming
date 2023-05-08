489e794f600c2aca6cf8cd2144d167440c16f9d7
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-08 13:39:44 -0700

    modified a file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,13 +11,14 @@ int main(void)
 {
 long n = 612852475143;
 long largest_prime_factor = 1;
+long i;
 
 while (n % 2 == 0) 
 {
 largest_prime_factor = 2;
 n /= 2;
 }
-for (long i = 3; i <= sqrt(n); i += 2) 
+for (i = 3; i <= sqrt(n); i += 2) 
 {
 while (n % i == 0) 
 {
