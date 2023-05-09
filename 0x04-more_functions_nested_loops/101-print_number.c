c917906440757320c67300fd47d41339b4db31f9
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-08 18:31:39 -0700

    modified a file

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,12 +1,26 @@
 #include "main.h"
 /**
 * print_number - Write a function that prints an integer.
-* @n parameter to check
+* @n: parameter to check
 * Return: 0 (success)
 */
 void print_number(int n)
 {
 int divisor = 1;
+if (n == INT_MIN)
+{
+putchar('-');
+putchar('2');
+putchar('1');
+putchar('4');
+putchar('7');
+putchar('4');
+putchar('8');
+putchar('3');
+putchar('6');
+putchar('4');
+putchar('8');
+}
 if (n < 0)
 {
 _putchar('-');
