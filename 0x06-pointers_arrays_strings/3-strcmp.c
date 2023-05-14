ed02b0c68eb359b760db1a251cfd37a972eb9c2d
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-14 12:50:48 -0700

    .

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1 ptr: string 1
+ * @s2 ptr: string 2
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+int i = 0;
+
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] < s2[i] || s1[i] > s2[i])
+{
+return (s1[i] - s2[i]);
+}
+i++;
+}
+}
