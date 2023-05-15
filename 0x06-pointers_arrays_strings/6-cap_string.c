eaaeebb28395cd534117988b0d6d89a83cf87682
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-14 17:26:20 -0700

    .

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ *
+ * @str ptr: string pointer
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *str)
+{
+int i, j;
+i = 0;
+while (str[i] != '\0')
+{
+i++;
+}
+str[i] = '\0';
+for (j = 0; j < i; j++)
+{
+switch (str[j])
+{
+case 32:
+case 9:
+case 10:
+case 44:
+case 59:
+case 46:
+case 33:
+case 63:
+case 34:
+case 123:
+case 125:
+case 40:
+case 41:
+if (str[j + 1] >= 97 && str[j + 1] <= 122)
+{
+str[j + 1] -= 32;
+};
+default:
+break;
+}
+}
+return (str);
+}
