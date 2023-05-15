ad0944857111cc8693ad183cb5a2245ecc20a375
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-15 06:39:04 -0700

    .

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,40 +4,21 @@
 /**
  * leet - encodes a string into 1337
  *
- * @str ptr: string pointer
+ * @s ptr: string pointer
  * Return: Always 0 (Success)
  */
-char *leet(char *str)
+char *leet(char *s)
 {
-int i, j;
-i = 0;
-while (str[i] != '\0')
+int i = 0, j;
+char subs[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+char str[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+
+while (s[i])
 {
+for (j = 0; j < 10; j++)
+if (s[i] == str[j])
+s[i] = subs[j];
 i++;
 }
-str[i] = '\0';
-for (j = 0; j < i; j++)
-{
-if (str[j] == 65 || str[j] == 97)
-{
-str[j] = 52;
-}
-else if (str[j] == 101 || str[j] == 69)
-{
-str[j] = 51;
-}
-else if (str[j] == 111 || str[j] == 79)
-{
-str[j] = 48;
-}
-else if (str[j] == 116 || str[j] == 84)
-{
-str[j] = 55;
-}
-else if (str[j] == 108 || str[j] == 76)
-{
-str[j] = 49;
-}
-}
-return (str);
+return (s);
 }
