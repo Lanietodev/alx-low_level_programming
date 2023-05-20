2a83ef3e1fff36cdce7e9dd8cf09197e10e2f794
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-19 19:23:41 -0700

    .

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -29,5 +29,5 @@ int indirect_sqrt_recursion(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i + 1));
+	return (indirect_sqrt_recursion(n, i + 1));
 }
