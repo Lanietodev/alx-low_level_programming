0a36f5d8a2f8246035d226f56d15f075bd8b14ad
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-24 20:34:18 -0700

    .

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - entry point
+ * @str ptr:  string pointer
+ * Return: a pointer to string
+*/
+char *_strdup(char *str)
+{
+int i = 0;
+int j;
+
+while (str[i] != '\0')
+{
+i++;
+}
+char *dup = (char *)malloc((i + 1) * sizeof(char));
+if (dup == NULL)
+{
+exit(0);
+}
+if (str == NULL)
+{
+return (NULL);
+}
+for (j = 0; j < i; j++)
+{
+dup[j] = str[j];
+}
+for (j = 0; j < i; j++)
+{
+return (dup);
+}
+free(dup);
+return (dup);
+}
