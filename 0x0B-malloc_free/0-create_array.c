052af9a0a19191ba309eb157bd1436b47353be0b
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-24 17:56:40 -0700

    .

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - entry point
+ * @size: size of array
+ * @c: character of array
+ * Return: a pointer to the array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+int i;
+
+char *array = (char *) malloc(size * sizeof(char));
+if (size == 0)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
+}
