fceb4f88aaa9b2e546ff6acedc2356c813b4f8fc
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-27 09:20:47 -0700

    .

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,9 +1,9 @@
-#include <stdio.h>		
+#include <stdio.h>
 #include <stdlib.h>
-/**		
-* free_grid - frees 2d array		
-* @grid: 2d grid		
-* @height: height dimension of grid		
+/**
+* free_grid - frees 2d array
+* @grid: 2d grid
+* @height: height dimension of grid
 * Return: nothing
 */
 void free_grid(int **grid, int height)
