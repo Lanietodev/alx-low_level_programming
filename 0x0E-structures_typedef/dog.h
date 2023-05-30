641249798a04a77b5e3c2d35680f3848aa485362
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-30 12:35:45 -0700

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure about dog's name, age and owner
+ * @name ptr: First member
+ * @age: Second member
+ * @owner ptr: Third member
+ */
+
 struct dog
 {
 	char *name;
