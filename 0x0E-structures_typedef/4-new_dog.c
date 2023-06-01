6f179c5c35115c881c0cc825ec897286ab68e163
Melanie Eureka Ngome <melanieeengome@gmail.com>
2023-05-31 18:34:22 -0700

    .

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,48 +1,49 @@
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
 
 /**
- * new_dog - Creates a new dog with the given name, age, and owner.
- *
- * @name:  The name of the dog.
- * @age:   The age of the dog.
- * @owner: The owner of the dog.
- *
- * Return: A pointer to the newly created dog structure, or NULL if
- *         memory allocation fails.
+ * new_dog - Function that creates a new dog.
+ * @name: type pointer char name.
+ * @age: type pointer float age
+ * @owner: type pointer char owner
+ * Return: 0.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i = 0, j = 0, k;
+	struct dog *techie;
+	char *i, *j;
+	int x, y, z;
 
-dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
-if (new_dog == NULL)
-return (NULL);
-while (name[i] != '\0')
-i++;
+	techie = malloc(sizeof(struct dog));
+	if (techie == NULL)
+		return (NULL);
 
-new_dog->name = (char *)malloc(i + 1);
-if (new_dog->name == NULL)
-{
-return (NULL);
-}
-for (k = 0; k <= i; k++)
-new_dog->name[k] = name[k];
+	for (x = 0; *(name + x) != '\0'; x++)
+		;
+	for (y = 0; *(owner + y) != '\0'; y++)
+		;
+	i = malloc(sizeof(char) * x + 1);
+	if (i == NULL)
+	{
+		free(techie);
+		return (NULL);
+	}
+	j = malloc(sizeof(char) * y + 1);
+	if (j == NULL)
+	{
+		free(i);
+		free(techie);
+		return (NULL);
+	}
+	for (z = 0; z <= x; z++)
+		*(i + z) = *(name + z);
+	for (z = 0; z <= y; z++)
+		*(j + z) = *(owner + z);
 
-new_dog->name[k] = '\0';
-while (owner[j] != '\0')
-j++;
+	techie->name = i;
+	techie->age = age;
+	techie->owner = j;
 
-new_dog->owner = (char *)malloc(j + 1);
-if (new_dog->owner == NULL)
-{
-return (NULL);
-}
-for (k = 0; k <= j; k++)
-new_dog->owner[k] = owner[k];
-
-new_dog->owner[k] = '\0';
-new_dog->age = age;
-return (new_dog);
+	return (techie);
 }
