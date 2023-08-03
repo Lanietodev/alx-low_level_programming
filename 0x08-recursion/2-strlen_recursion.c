68f8b3a899266aa6d01bacec56fa50cba1df4729
Melanie Ngome <Melanieeengome@gmail.com>
2023-08-02 22:05:20 -0700

    created a filee

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strlen_recursion - Entry point
+ * @s ptr: input
+ * Return: Always 0 (Success)
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+else
+{
+return 1 + _strlen_recursion(s + 1);
+}
+}
