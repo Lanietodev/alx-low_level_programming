c03cb380a1426cff4bc5198a43addcef22a37df6
Melanie Ngome <melanieeengomee@example.com>
2023-08-05 07:04:21 -0700

    created fourth task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv ptr: argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+int i, mul = 1;
+if (argc > 1)
+{
+for (i = 1; i < argc; i++)
+{
+mul *= atoi(argv[i]);
+}
+printf("%d\n", mul);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main
+ *
+ *Desc: header file
+ */
+
 int _putchar(char c);
+int _atoi(char *s);
+
+#endif
