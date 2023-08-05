8ee22c4b9754980bdf3c800bfb67568b564fef71
Melanie Ngome <melanieeengomee@example.com>
2023-08-05 05:47:58 -0700

    Created first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main (int argc, char *argv[])
+{
+if (argc >= 1) {
+printf("%s\n", argv[0]);
+}
+return (0);
+}
