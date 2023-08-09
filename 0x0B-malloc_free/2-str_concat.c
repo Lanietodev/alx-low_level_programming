e4ebdd3f94d2c7768b8668ca11e23e798e0585f8
Melanie Ngome <melanieeengomee@example.com>
2023-08-08 18:31:47 -0700

    Created third task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*str_concat - Entry point
+*@s1: pointer to first string
+*@s2: pointer to second string
+*Return: pointer to concatenated string
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int i, j, k, m;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	i = 0;
+	 while (s1[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc((sizeof(char)) * (i + j + 1));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+	{
+		ptr[k] = s1[k];
+	}
+	for (m = 0; m < j; m++)
+	{
+		ptr[i + m] = s2[m];
+	}
+	ptr[i + m] = '\0';
+	return (ptr);
+}
