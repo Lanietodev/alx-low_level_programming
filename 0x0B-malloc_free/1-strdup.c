dd096210edb65231581d2ce0e66d2f0f82e9698d
Melanie Ngome <melanieeengomee@example.com>
2023-08-08 17:57:22 -0700

    created second task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*create_array - Entry point
+*@str: pointer to string
+*Return: pointer to string
+*/
+
+char *_strdup(char *str)
+{
+	unsigned int i, j = 0;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	ptr = malloc (sizeof(char) * (j + 1));
+
+	if (ptr == NULL)
+	{
+		return (NULL)
+	}
+	for (i = 0; i < j; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[j] = '\0';
+	return (ptr);
+}
