37922e5244d42bf4b27ff2e0a5735917e7292b3a
Melanie Ngome <melanieeengomee@example.com>
2023-08-20 13:04:13 -0700

    Completed tasks

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,2 +1,5 @@
 void *malloc_checked(unsigned int b);
 int _putchar(char c);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
