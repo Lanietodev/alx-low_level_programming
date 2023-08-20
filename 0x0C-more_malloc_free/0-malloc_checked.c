e72fd2cfd42e5a75978fa4a1e9a34b6ca2319b4b
Melanie Ngome <melanieeengomee@example.com>
2023-08-20 12:13:04 -0700

    Completed first task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,12 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b) {
+  void *ptr;
+  ptr = malloc(b);
+  if (ptr == NULL) {
+    exit(98);
+  }
+  return (ptr);
+}
