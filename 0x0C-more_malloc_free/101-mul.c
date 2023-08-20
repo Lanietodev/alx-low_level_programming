5c536e32df8ef618ecf4ab6da91fee56a5231074
Melanie Ngome <melanieeengomee@example.com>
2023-08-20 14:01:10 -0700

    Modified tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,13 +9,13 @@
  * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s) {
-  int i = 0;
+	int i = 0;
 
-  while (s[i]) {
-    if (s[i] < '0' || s[i] > '9') return (0);
-    i++;
-  }
-  return (1);
+	while (s[i]) {
+	if (s[i] < '0' || s[i] > '9') return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
@@ -25,20 +25,20 @@ int is_digit(char *s) {
  * Return: the length of the string
  */
 int _strlen(char *s) {
-  int i = 0;
+	int i = 0;
 
-  while (s[i] != '\0') {
-    i++;
-  }
-  return (i);
+	while (s[i] != '\0') {
+		i++;
+	}
+	return (i);
 }
 
 /**
  * errors - handles errors for main
  */
 void errors(void) {
-  printf("Error\n");
-  exit(98);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
@@ -49,8 +49,8 @@ void errors(void) {
  * Return: always 0 (Success)
  */
 int main(int argc, char *argv[]) {
-  char *s1, *s2;
-  int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2)) errors();
