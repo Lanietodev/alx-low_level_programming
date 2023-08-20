0d1e9004f9f16f100bcc3fdeddeb8bcea6d8aa10
Melanie Ngome <melanieeengomee@example.com>
2023-08-20 12:18:30 -0700

    Modified first task

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void *malloc_checked(unsigned int b) {
-  void *ptr;
-  ptr = malloc(b);
-  if (ptr == NULL) {
-    exit(98);
-  }
-  return (ptr);
+/**
+ * malloc_checked - Entry point
+ * @b: Size to check
+ * Return: Pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
 }
