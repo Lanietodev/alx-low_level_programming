8d05081c74da9ad2ce773687988027a898f14285
Melanie Ngome <melanieeengomee@example.com>
2023-08-20 22:11:11 -0700

    .

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,19 +9,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i;
-    int sum = 0;
-    va_list ptr;
+	unsigned int i;
+	int sum = 0;
+	va_list ptr;
 
-    if (n == 0)
-    {
-        return (0);
-    }
-    va_start(ptr, n);
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(ptr, int);
-    }
-    va_end(ptr);
-    return (sum);
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum);
 }
